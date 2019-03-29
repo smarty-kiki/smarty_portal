6aa78330f37bde1ceca47be2273f50b8cb4d293d
yaoyang <kiki@smarty.so>
2019-03-29 10:41:18 +0800

    添加 afterpush 脚本,添加域名

---
+++ project/tool/after_push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../..
+
+ln -fs $ROOT_DIR/project/config/production/nginx/smarty_portal.conf /etc/nginx/sites-enabled/smarty_portal
+/usr/sbin/service nginx reload
+
+/bin/bash $ROOT_DIR/project/tool/dep_build.sh link
+/usr/bin/php $ROOT_DIR/public/cli.php migrate
