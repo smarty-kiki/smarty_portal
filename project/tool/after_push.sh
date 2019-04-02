22ee9eb2fdd905e81c86da3b39141c99679d3961
yaoyang <kiki@smarty.so>
2019-04-02 16:15:15 +0800

    添加发布后的清 blade 缓存动作

--- project/tool/after_push.sh
+++ project/tool/after_push.sh
@@ -9,3 +9,4 @@ ln -fs $ROOT_DIR/project/config/production/nginx/smarty_portal.conf /etc/nginx/s
 /usr/bin/php $ROOT_DIR/public/cli.php migrate
 
 chown -R www-data:www-data /var/www/smarty_portal/view/blade
+rm -rf /var/www/smarty_portal/view/blade/*.php
