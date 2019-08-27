bcc979ae786786d3b6b1ad05b77cfe3ba2a1e017
yaoyang <kiki@smarty.so>
2019-08-28 00:34:14 +0800

    after_dev_env_start 命令添加 env 参数

--- project/tool/after_dev_env_start.sh
+++ project/tool/after_dev_env_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-php /var/www/smarty_portal/public/cli.php migrate:install
-php /var/www/smarty_portal/public/cli.php migrate
+ENV=development php /var/www/smarty_portal/public/cli.php migrate:install
+ENV=development php /var/www/smarty_portal/public/cli.php migrate
 
-php /var/www/smarty_portal/public/cli.php seed
+ENV=development php /var/www/smarty_portal/public/cli.php seed
